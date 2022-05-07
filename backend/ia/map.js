8fb4133ab959442797b5b87d81927d97f160e16c
filip-769 <filip769@proton.me>
2022-05-07 12:10:33 +0200

    small change

--- backend/ia/map.js
+++ backend/ia/map.js
@@ -1,11 +1,11 @@
 export default async q => {
-    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(q.replace( /(^| )map($| )/i, "").trim())}&count=1`);
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(q.replace(/(^| )(map|where is|location)($| )/i, "").trim())}&count=1`);
     const json = await response.json();
 
     if(!(json?.results?.length >= 1)) return false;
 
     return {
         name: json.results[0].name,
-        img: `/proxy?url=${encodeURIComponent(`https://external-content.duckduckgo.com/ssv2/?center=${json.results[0].latitude},${json.results[0].longitude}&colorScheme=dark&spn=1,1`)}`
+        img: `/proxy?url=${encodeURIComponent(`https://external-content.duckduckgo.com/ssv2/?center=${json.results[0].latitude},${json.results[0].longitude}&lang=en&annotations=[{"point":"${json.results[0].latitude},${json.results[0].longitude}","color":"teal"}]&spn=50,50`)}`
     }
 }
