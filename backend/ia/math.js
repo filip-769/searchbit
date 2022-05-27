0ba69d1f822abcac356b73afaeb9056dc6caeb83
filip-769 <77580207+filip-769@users.noreply.github.com>
2022-05-27 17:26:21 +0000

    bug fix

--- backend/ia/math.js
+++ backend/ia/math.js
@@ -2,6 +2,8 @@ export default async q => {
     const response = await fetch(`https://api.mathjs.org/v4/?expr=${encodeURIComponent(q.replaceAll(",", "."))}`);
     const data = await response.text();
 
+    if(data.startsWith("Error:")) return;
+
     return {
         input: q.replaceAll(",", "."),
         result: data
